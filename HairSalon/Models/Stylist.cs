0214e30772e59a800411255c8b01326be4baf4cf
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-05-19 09:11:59 -0700

    add properties to Stylist model.

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -4,6 +4,9 @@ namespace HairSalon.Models
 {
   public class Stylist
   {
-
+    public string Name { get; set; }
+    public string Speciality { get; set; }
+    public int StylistId { get; }
+    public List<Client> Clients { get; set; }
   }
 }
