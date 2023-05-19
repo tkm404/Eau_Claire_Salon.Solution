e7a47cc6d3d088c4e3ec2e7578a4a65667542505
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-05-19 10:03:00 -0700

    add Create path to Client object.

--- HairSalon/Controllers/ClientController.cs
+++ HairSalon/Controllers/ClientController.cs
@@ -5,20 +5,34 @@ using System.Linq;
 
 namespace HairSalon.Controllers
 {
-    public class ClientController : Controller
-{
+public class ClientController : Controller
+  {
     private readonly HairSalonContext _db;
 
     public ClientController(HairSalonContext db)
     {
-        _db = db;
+      _db = db;
     }
 
-  public ActionResult Index()
+    public ActionResult Index()
     {
-        List<Client> model = _db.Clients.ToList();
-        return View(model);
+      List<Client> model = _db.Clients.ToList();
+      return View(model);
     }
 
-}
+		public ActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult Create(Client client)
+		{
+			_db.Clients.Add(client);
+			_db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+
+  }
 }
