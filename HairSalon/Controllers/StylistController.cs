80d6248fc838e26a7f0c0f2aed00abd6c7cd434f
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-05-19 11:05:42 -0700

    add routing paths in Stylist controller.

--- HairSalon/Controllers/StylistController.cs
+++ HairSalon/Controllers/StylistController.cs
@@ -5,19 +5,66 @@ using System.Linq;
 
 namespace HairSalon.Controllers
 {
-    public class StylistController : Controller
-{
- private readonly HairSalonContext _db;
+public class StylistController : Controller
+  {
+    private readonly HairSalonContext _db;
 
     public StylistController(HairSalonContext db)
     {
-        _db = db;
+      _db = db;
     }
-  public ActionResult Index()
+    public ActionResult Index()
     {
-        List<Stylist> model = _db.Stylists.ToList();
-        return View(model);
+      List<Stylist> model = _db.Stylists.ToList();
+      return View(model);
     }
 
-}
+			public ActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult Create(Stylist stylist)
+		{
+			_db.Stylists.Add(stylist);
+			_db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+		public ActionResult Details(int id)
+		{
+			Stylist thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistsId == id);
+			return View(thisStylist);
+		}
+
+		public ActionResult Edit(int id)
+		{
+			Stylist thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistsId == id);
+			return View(thisStylist);
+		}
+		[HttpPost]
+		public ActionResult Edit(Stylist stylist)
+		{
+			_db.Stylists.Update(stylist);
+			_db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+		public ActionResult Delete(int id)
+		{
+			Stylist thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistsId == id);
+			return View(thisStylist);
+		}
+
+		[HttpPost, ActionName("Delete")]
+		public ActionResult DeleteConfirmed(int id)
+		{
+			Stylist thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistsId == id);
+			_db.Stylists.Remove(thisStylist);
+			_db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+  }
 }
