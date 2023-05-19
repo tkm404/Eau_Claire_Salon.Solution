d064c05171438eeda780af678776bed8cf370647
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-05-19 09:30:56 -0700

    add READ path to client and stylist controllers.

--- HairSalon/Controllers/ClientController.cs
+++ HairSalon/Controllers/ClientController.cs
@@ -1,14 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace HairSalon.Controllers
 {
     public class ClientController : Controller
 {
+    private readonly HairSalonContext _db;
+
+    public ClientController(HairSalonContext db)
+    {
+        _db = db;
+    }
 
   public ActionResult Index()
     {
-        return View();
+        List<Client> model = _db.Clients.ToList();
+        return View(model);
     }
 
 }
--- HairSalon/Controllers/StylistController.cs
+++ HairSalon/Controllers/StylistController.cs
@@ -1,14 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
 using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace HairSalon.Controllers
 {
     public class StylistController : Controller
 {
+ private readonly HairSalonContext _db;
 
+    public StylistController(HairSalonContext db)
+    {
+        _db = db;
+    }
   public ActionResult Index()
     {
-        return View();
+        List<Stylist> model = _db.Stylists.ToList();
+        return View(model);
     }
 
 }
