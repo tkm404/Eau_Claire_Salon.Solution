8c4026cc2ab6e3130012ea3b27d97288f46c940e
tkm404 <127699298+tkm404@users.noreply.github.com>
2023-05-21 22:04:10 -0700

    Update HairSalonContext.cs

--- HairSalon/Models/HairSalonContext.cs
+++ HairSalon/Models/HairSalonContext.cs
@@ -10,6 +10,3 @@ namespace HairSalon.Models
     public HairSalonContext(DbContextOptions options) : base(options) { }
   }
 }
-
-// Rename all [ Project Names ]; Rename Primary and Secondary Models with whatever model names you need. You may not need both, so use your best judgement.
-// You only need this file if you're working with a database.
